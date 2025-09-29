bf8da2a8bbe9b1f760d650ac4793cf2895be2914
yihaochen0118 <yihaochen0118@gmail.com>
2025-09-29 23:58:15 +0100

    add textscript

--- the-dicebound-blade/Script/playerData.gd
+++ the-dicebound-blade/Script/playerData.gd
@@ -7,10 +7,10 @@ var hp: int = 100
 var max_hp: int = 100
 
 var stats = {
-	"strength": 5,   # 力量 +5
+	"strength": 2,   # 力量 +5
 	"dexterity": 2,  # 敏捷 +1
 	"constitution": 4, # 体质 +3
-	"charisma": 6 #魅力 6
+	"charisma": 1 #魅力 6
 }
 
 
